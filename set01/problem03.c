b2d490eef901004bc24ca96df4dfb87b24480ed7
Donna R <117378972+DonnaR28@users.noreply.github.com>
2022-12-15 09:51:27 +0000

    problem03 solved again

--- set01/problem03.c
+++ set01/problem03.c
@@ -3,23 +3,23 @@ int input();
 int add(int a, int b);
 void output(int a, int b, int sum);
 int main(){
-  int a, b, sum;
-  printf("enter two no's: ");
+  int a,b,sum;
   a = input();
   b = input();
-  sum = add(a, b);
+  sum = add(a,b);
   output(a, b, sum);
 }
 int input(){
   int a;
-  if(scanf("%d", &a))
-  return a;
+  printf("Enter a value: ");
+  if(scanf("%d",&a));
+   return a;
 }
 int add(int a, int b){
   int sum;
-  sum = a + b;
+  sum = a+b;
   return sum;
-}
- void output(int a, int b, int sum){
-   printf("The sum of %d and %d is: %d\n", a, b,sum);
+} 
+void output(int a, int b, int sum){
+  printf("The sum of %d and %d is %d", a,b,sum);
 }
