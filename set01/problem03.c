304067b7699fadb010685eba0961c6399f36ea59
Donna R <117378972+DonnaR28@users.noreply.github.com>
2023-01-03 05:44:56 +0000

    done again

--- set01/problem03.c
+++ set01/problem03.c
@@ -1,25 +1,24 @@
 #include<stdio.h>
 int input();
 int add(int a, int b);
-void output(int a, int b, int sum);
+void output(int a , int b, int sum);
 int main(){
-  int a,b,sum;
+  int a, b, sum;
   a = input();
-  b = input();
-  sum = add(a,b);
-  output(a, b, sum);
+  b =input();
+  sum = add(a, b);
+  output(a,b,sum);
 }
 int input(){
   int a;
-  printf("Enter a value: ");
-  if(scanf("%d",&a));
-   return a;
+  if(scanf("%d", &a));
+  return a;
 }
-int add(int a, int b){
+int add(int a , int b){
   int sum;
   sum = a+b;
   return sum;
-} 
+}
 void output(int a, int b, int sum){
-  printf("The sum of %d and %d is %d", a,b,sum);
+  printf("The sum of %d and %d is %d\n", a, b, sum);
 }
