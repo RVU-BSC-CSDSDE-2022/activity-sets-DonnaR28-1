e87b8f894057cc5365c6226264be8ca5bc80af6d
Donna R <117378972+DonnaR28@users.noreply.github.com>
2023-01-03 07:10:33 +0000

    done again

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,5 +1,5 @@
-#include<stdio.h> //take contents of stdio.h file and put it in the output file 
-int input(); //function prototype ==> to check whether the function call passes the right number of arguments and type of arguments.
+#include<stdio.h>
+int input();
 int compare(int a, int b, int c);
 void output(int a, int b, int c, int largest);
 int main(){
@@ -8,17 +8,21 @@ int main(){
   b = input();
   c = input();
   largest = compare(a,b,c);
-  output(a,b,c,largest);
+  output(a, b, c, largest);
 }
 int input(){
   int a;
-  printf("Enter a number: ");
-  if(scanf("%d",&a));
+  if(scanf("%d", &a));
   return a;
 }
 int compare(int a, int b, int c){
-  return a>b?(a>c?a:c):(b>c?b:c); // ? is to chech whether it is true or not.
+  if(a>b && a>c)
+    return a;
+  if(b>c && b>a)
+    return b;
+  else
+    return c;
 }
 void output(int a, int b, int c, int largest){
-  printf("The largest of %d, %d and %d is %d \n", a, b, c, largest);
+  printf("the largest of %d, %d and %d is %d\n", a, b, c, largest);
 }
