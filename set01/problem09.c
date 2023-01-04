641f6e8219f337e5327ab1d066782a64e052d124
Donna R <117378972+DonnaR28@users.noreply.github.com>
2023-01-04 09:03:23 +0000

    done again

--- set01/problem09.c
+++ set01/problem09.c
@@ -1,32 +1,29 @@
-#include<math.h>
-#include<stdio.h>
+#include <math.h>
+#include <stdio.h>
 float input();
 float square_root(float n);
 void output(float n, float sqrroot);
-int main()
-{
+int main() {
   float n, sqrroot;
-  printf("Enter number: ");
-  n =input();
+  printf("Enter a value: ");
+  n = input();
   sqrroot = square_root(n);
   output(n, sqrroot);
-  return 0;
 }
-float input(){
+float input() {
   float n;
-  scanf("%f",&n);
+  if(scanf("%f", &n));
   return n;
 }
-float square_root(float n){
-  float sqrroot, temp;
-  sqrroot = n/2;
-  temp = 0;
-  while( sqrroot != temp){
-    temp = sqrroot;
-    sqrroot = (n/temp+temp)/2;
+float square_root(float n) {
+  float s, x,y;
+  s = n;
+  x = 1;
+  while(fabs(x * x -s )>0.0001){
+    x = (x + s/x)/2;
   }
-  return sqrroot;
+  return x;
 }
-void output(float n, float sqrroot){
-  printf("Square root of %f is %f", n, sqrroot);
+void output(float n, float sqrroot) {
+  printf("Square root of %.2f is %.2f\n", n, sqrroot);
 }
