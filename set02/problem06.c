c842af908bcabf0e050ab11786727e72f7d2fe32
Donna R <117378972+DonnaR28@users.noreply.github.com>
2023-01-17 09:16:29 +0000

    6 done new

--- set02/problem06.c
+++ set02/problem06.c
@@ -1,33 +1,26 @@
-#include <stdio.h>  
-#include <string.h>  
-  
-// use recursion function   
-void revstr(char *str1)  
-{  
-    // declare static variable  
-    static int i, len, temp;  
-    len = strlen(str1); // use strlen() to get the length of str string  
-      
-      
-    if (i < len/2){  
-        // temp variable use to temporary hold the string  
-        temp = str1[i];  
-        str1[i] = str1[len - i - 1];  
-        str1[len - i - 1] = temp;  
-        i++;  
-        revstr(str1); // recusively calls the revstr() function  
-    }  
-}  
-      
-    int main()  
-    {  
-        char str1[50]; // size of char string  
-        printf (" Enter the string: ");  
-        gets(str1); // use gets() function to take string  
-          
-        printf (" \n Before reversing the string: %s \n", str1);  
-          
-        // call revstr() function   
-        revstr(str1);  
-        printf (" After reversing the string: %s", str1);  
-    }  
+#include<stdio.h>
+void input_string(char *a);
+void str_reverse(char *str, char *rev_str);
+void output(char *a, char *reverse_a);
+int main(){
+  char a[100], b[100];
+  input_string(a);
+  str_reverse(a, b);
+  output(a, b);
+}
+void input_string(char *a){
+  printf("Enter a string: ");
+  if(scanf("%s", a));
+}
+void str_reverse(char *str, char *rev_str){
+  int i=0, j=0, len;
+  for(i = 0; str[i]!='\0'; i++);
+  len = i--;
+  for(int i = len-1; i>=0; i--){
+      rev_str[j] = str[i];
+      j++;
+    }
+}
+void output(char *a, char *reverse_a){
+  printf("the reverse string is : %s\n", reverse_a);
+}
